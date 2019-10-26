86140abb01348a1535c1252f29d6e668c0e11dc0
lauramasselos <s1603459@sms.ed.ac.uk>
2019-10-26 13:27:46 +0100

    Fixed typo

--- setup.sh
+++ setup.sh
@@ -19,9 +19,8 @@ git clone https://github.com/google/googletest.git
 
 cd googletest && mkdir build && cd build
 
-cmake -DCMAKE_CXX_COMPILER="c++" -DCMAKE_CXX_FLAGS="-std=c++11 =stdlib=libc++" ..
+cmake -DCMAKE_CXX_COMPILER="c++" -DCMAKE_CXX_FLAGS="-std=c++11 -stdlib=libc++" ..
 
-#cmake ..
 
 make
 make install
